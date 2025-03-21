6aa47edddc3acfb17449c66a2effd0e34fd2a6f6
Sebastien Cros <secros@z2r9p6.42lyon.fr>
2025-03-21 13:46:27 +0100

    Small fix

--- inc/bt_malloc.h
+++ inc/bt_malloc.h
@@ -1,31 +1,36 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   better_mallocs.h                                   :+:      :+:    :+:   */
+/*   bt_malloc.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: secros <secros@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/17 16:18:31 by secros            #+#    #+#             */
-/*   Updated: 2025/03/17 16:21:37 by secros           ###   ########.fr       */
+/*   Updated: 2025/03/21 13:45:27 by secros           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef BETTER_MALLOCS_H
-# define BETTER_MALLOCS_H
+#ifndef BT_MALLOC_H
+# define BT_MALLOC_H
 
-typedef struct s_garb
+# include "libft.h"
+
+typedef struct s_sink
 {
 	void			*content;
 	void			(*f)(void *);
-	struct s_garb	*next;
-}	t_garb;
+	struct s_sink	*next;
+}	t_sink;
+
+//utils
+t_sink	**get_sink(t_sink **bin);
 
 //create
-void	*add_garbage(void *pt, void (*free_pt)(void *), t_garb **head);
-void	*ft_malloc(size_t size, t_garb **bin);
+void	*fill_dishwasher(void *pt, void (*free_pt)(void *), t_sink **head);
+void	*new_plate(size_t size, t_sink **bin);
 
 //destroy
-void	ft_free(void *pt, t_garb **head);
-void 	clear_garbage(t_garb **head);
+void	do_dishes(t_sink **head);
+void	hand_wash(void *pt, t_sink **head);
 
 #endif
