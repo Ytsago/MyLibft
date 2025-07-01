ad62b300f5d85ffd5cac1924a7cefee3b79aa20e
Sebastien Cros <secros@z3r11p5.42lyon.fr>
2025-07-01 17:45:13 +0200

    new bzero

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: secros <secros@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/05 10:41:21 by secros            #+#    #+#             */
-/*   Updated: 2025/06/29 12:17:59 by secros           ###   ########.fr       */
+/*   Updated: 2025/06/29 11:47:39 by secros           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,7 +35,6 @@ int		ft_tolower(int c);
 int		ft_toupper(int c);
 
 void	ft_bzero(void *s, size_t n);
-void	ft_fbzero(void *s, size_t n);
 void	*ft_memset(void *s, int c, size_t n);
 void	*ft_memcpy(void *dest, const void *src, size_t n);
 void	*ft_memmove(void *dest, const void *src, size_t n);
--- src/ft_bzero.c
+++ src/ft_bzero.c
@@ -6,7 +6,7 @@
 /*   By: secros <secros@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/05 10:39:38 by secros            #+#    #+#             */
-/*   Updated: 2024/11/05 13:56:03 by secros           ###   ########.fr       */
+/*   Updated: 2025/06/29 12:17:47 by secros           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,5 +21,34 @@ void	ft_bzero(void *s, size_t n)
 	{
 		((unsigned char *)s)[i] = '\0';
 		i++;
+	}
+}
+
+void	ft_fbzero(void *s, size_t n)
+{
+	if (n < MIN_SIZE)
+		return (ft_bzero(s, n));
+	while ((uintptr_t)s % sizeof(uint64_t) != 0 && n-- > 0)
+	{
+		*((uint8_t *)s) = 0;
+		s = (uint8_t *)s + 1;
+	}
+	while (n >= sizeof(uint64_t))
+	{
+		*((uint64_t *)s) = 0;
+		s = (uint8_t *)s + sizeof(uint64_t);
+		n -= sizeof(uint64_t);
+	}
+	if (n >= sizeof(uint32_t))
+	{
+		*((uint32_t *)s) = 0;
+		s = (uint8_t *)s + sizeof(uint32_t);
+		n -= sizeof(uint32_t);
+	}
+	while (n > 0)
+	{
+		*((uint8_t *)s) = 0;
+		s = (uint8_t *)s + 1;
+		n--;
 	}
 }
